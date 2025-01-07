12a425741036dea237a66be24ef5ae25ad10998b
Emad Altaki <s.emadaltaki@gmail.com>
2025-01-07 12:53:09 -0500

    Update investment_calculater.go

--- investment_calculater.go
+++ investment_calculater.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturn = 5.5
-	var years = 10
+	var investmentAmount float64 = 1000
+	expectedReturn := 5.0
+	years := 10
 	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturn/100, float64(years))
 	fmt.Println(futureValue)
 }
