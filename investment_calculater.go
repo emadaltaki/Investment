32143d2f579ce85f0f50c069b0c9ed91e20368df
Emad Altaki <s.emadaltaki@gmail.com>
2025-01-07 14:23:09 -0500

    Added the futureValue after Inflation

--- investment_calculater.go
+++ investment_calculater.go
@@ -6,9 +6,12 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	var investmentAmount float64 = 1000
 	expectedReturn := 5.0
 	years := 10
 	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturn/100, float64(years))
+	futureRealValue = futureValue / math.Pow(1+inflationRate/100, float64(years))
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
