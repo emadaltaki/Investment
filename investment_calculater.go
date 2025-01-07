dd5d33e77883fa5e06630e91adf1ad5edac7c4e4
Emad Altaki <s.emadaltaki@gmail.com>
2025-01-07 14:56:31 -0500

    Fetch user Input using fmt.Scan()

--- investment_calculater.go
+++ investment_calculater.go
@@ -6,12 +6,14 @@ import (
 )
 
 func main() {
-	const inflationRate = 2.5
-	var investmentAmount float64 = 1000
-	expectedReturn := 5.0
+	const inflationRate float64 = 2
+	var investmentAmount float64
+	expectedReturn := 20.0
 	years := 10
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturn/100, float64(years))
-	futureRealValue = futureValue / math.Pow(1+inflationRate/100, float64(years))
+	a, v := fmt.Scan(&investmentAmount, &expectedReturn, &years)
+	fmt.Println(a, v)
+	futureValue := float64(investmentAmount) * math.Pow(1+expectedReturn/100, float64(years))
+	futureRealValue := futureValue / (math.Pow(1+inflationRate/100, float64(years)))
 	fmt.Println(futureValue)
 	fmt.Println(futureRealValue)
 }
