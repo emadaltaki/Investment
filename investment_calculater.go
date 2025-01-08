a8b5188bec253227231f87d579e04eca28f71201
Emad Altaki <s.emadaltaki@gmail.com>
2025-01-08 15:40:38 -0500

    Added taxes calculator

--- investment_calculater.go
+++ investment_calculater.go
@@ -1,25 +1,42 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	const inflationRate float64 = 2
-	expectedReturn := 5.5
-	var investmentAmount float64
-	var years float64
+	// Invetsment Calculater
+	// const inflationRate float64 = 2
+	// expectedReturn := 5.5
+	// var investmentAmount float64
+	// var years float64
 
-	fmt.Print("Enter the investment amount: ")
-	fmt.Scan(&investmentAmount)
-	fmt.Print("Enter the expected Return Rate: ")
-	fmt.Scan(&expectedReturn)
-	fmt.Print("Enter the number of years: ")
-	fmt.Scan(&years)
+	// fmt.Print("Enter the investment amount: ")
+	// fmt.Scan(&investmentAmount)
+	// fmt.Print("Enter the expected Return Rate: ")
+	// fmt.Scan(&expectedReturn)
+	// fmt.Print("Enter the number of years: ")
+	// fmt.Scan(&years)
 
-	futureValue := float64(investmentAmount) * math.Pow(1+expectedReturn/100, float64(years))
-	futureRealValue := futureValue / (math.Pow(1+inflationRate/100, float64(years)))
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	// futureValue := float64(investmentAmount) * math.Pow(1+expectedReturn/100, float64(years))
+	// futureRealValue := futureValue / (math.Pow(1+inflationRate/100, float64(years)))
+	// fmt.Println(futureValue)
+	// fmt.Println(futureRealValue)
+	/////////////////////////
+	//Tax Calculater
+
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+	fmt.Print("Enter the revenue: ")
+	fmt.Scan(&revenue)
+	fmt.Print("Enter the expenses: ")
+	fmt.Scan(&expenses)
+	fmt.Print("Enter the tax rate: ")
+	fmt.Scan(&taxRate)
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+
+	fmt.Println(ebt)
+	fmt.Println(profit)
+	fmt.Println(ratio)
 }
